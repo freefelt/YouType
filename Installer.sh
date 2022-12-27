e0a658c121d9b05ab0df76d9cfe76d2c43bb7a3b
Alexander Korotkov <eiros@me.com>
2022-12-27 13:56:57 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,18 +4,37 @@ CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NOCOLOR='\033[0m'
 
-printf "${CYAN}============================ Installing YouType =============================\n"
-
 if [[ $(dirname "$0") == /opt/homebrew/Caskroom/youtype* ]]; then
     FROMHOMEBREW=1
-    ARCHFILENAME="YouType.zip"
     APPDESTINATION="$(dirname "$0")"
 else
     FROMHOMEBREW=0
-    ARCHFILENAME="/tmp/YouType.zip"
     APPDESTINATION="/Applications"
 fi
+ARCHFILENAME="/tmp/YouType.zip"
 APPFILENAME="$APPDESTINATION/YouType.app"
+
+#---------------------------------------------------------------------------------------------------
+
+printf "${CYAN}============================ Installing YouType =============================\n"
+
+if test -d $APPFILENAME; then
+    printf "${WHITE}YouType is already installed. Would you like to reinstall? (y/n): "
+    read REINSTALL
+    if [[ $REINSTALL == "y" || $REINSTALL == "Y" ]]; then
+        CONTINUE=1
+        tput cuu 1; tput el;
+    else
+        CONTINUE=0
+    fi
+else
+    CONTINUE=1
+fi
+
+if [[ $CONTINUE == 0 ]]; then
+    printf "${CYAN}=============================================================================\n"
+    exit
+fi
 
 printf "${WHITE}- Downloading ${RED}"
 curl -fsSL -o $ARCHFILENAME 'https://github.com/freefelt/YouType/raw/main/YouType.zip'
@@ -24,30 +43,31 @@ if test -f $ARCHFILENAME; then
     printf "${GREEN}DONE\n"
     printf "${WHITE}- Unarchiving "
     
+    pkill -x YouType
+    
     unzip -qq -o $ARCHFILENAME -d $APPDESTINATION
     if test -d $APPFILENAME; then
         printf "${GREEN}DONE\n"
         
-        rm $ARCHFILENAME
         if [[ $(xattr $APPFILENAME) = *com.apple.quarantine* ]]; then
             printf "${WHITE}- Removing com.apple.quarantine attribute\n"
             xattr -d com.apple.quarantine $APPFILENAME
         fi
         
-        printf "${WHITE}- Removing com.apple.quarantine attribute\n"
-        
-#        if [[ $FROMHOMEBREW == 0 ]]; then
-#            printf "${NOCOLOR}Would you like to run YouType? (y/n): "
-#            read RUN
-#            if [[ $RUN == "y" || $RUN == "Y" ]]; then
-#                open -a YouType
-#            fi
-#        fi
-        
-        printf "${GREEN}  ** SUCCESS **\n"
+        if [[ $FROMHOMEBREW == 0 ]]; then
+            open -R $APPFILENAME
+        fi
     else
-        printf "${RED}Can't unzip YouType\n"
+        printf "${RED}FAILED\n"
     fi
+    
+    rm $ARCHFILENAME
+fi
+
+if test -d $APPFILENAME; then
+    printf "${GREEN}  ** SUCCESS **\n"
+else
+    printf "${RED}  ** FAIL **\n"
 fi
 
 printf "${CYAN}=============================================================================\n"
